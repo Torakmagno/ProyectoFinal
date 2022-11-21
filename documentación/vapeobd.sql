251f1e051f8ff10bc7e598b0a364847a4863f68f
TorakMagno <91882681+Torakmagno@users.noreply.github.com>
2022-11-21 18:06:25 +0100

    ModProducto-AmpliarBBDD

--- documentación/vapeobd.sql
+++ documentación/vapeobd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2022 a las 10:41:01
+-- Tiempo de generación: 21-11-2022 a las 18:05:11
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.12
 
@@ -75,24 +75,33 @@ CREATE TABLE `liquidos` (
   `descripcion` varchar(255) NOT NULL,
   `id` int(3) NOT NULL,
   `imagenId` varchar(255) DEFAULT NULL,
-  `categoria` enum('Tabaquil','Frutal','Postre') NOT NULL
+  `categoria` enum('Tabaquil','Frutal','Postre') NOT NULL,
+  `Cantidad` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `liquidos`
 --
 
-INSERT INTO `liquidos` (`nombre`, `marca`, `precio`, `descripcion`, `id`, `imagenId`, `categoria`) VALUES
-('Don Juan Reserva', 'King\'s Crest', 22, 'Postre de galleta con chocolate y vainilla.No podrás resistir!', 1, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1666704030/Productos/don_juan_pjtms8.jpg', 'Postre'),
-('Baker\'s Daughter', 'Coil sill hand crafted', 18, 'Liquido a sabor Tarta de la abuela!!!', 2, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/bakersDaughter_tzalrr.png', 'Postre'),
-('Kastard', 'Tonino Vaper', 17, 'Crema pastelera con caramelo, vainilla y canela.', 3, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/kastard_rbuabs.jpg', 'Postre'),
-('Salted Caramel', 'Nitro\'s Col Brew', 19, 'Caramelo salado con un toque de frescor.', 4, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427265/Productos/nitros_kzij3a.jpg', 'Postre'),
-('Strawberry Dreams', 'Best Vape', 21, 'Nata fresca casera con fresas de temporada.', 5, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427265/Productos/strawberry_yzciwp.jpg', 'Postre'),
-('Raspberry and Strawberry Cake', 'Queen of the Drips', 16, 'Tarta de galleta con mermelada de fresa y frambuesa', 6, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1666704031/Productos/raspberry_ti7ykj.jpg', 'Postre'),
-('Tormento', 'Alquimia', 15, 'Tabaco rubio de la mejor calidad con matices de coco y frutos secos.', 7, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1666704031/Productos/tormento_s4w16a.jpg', 'Tabaquil'),
-('Jungle Wave', 'Malasia vaps', 13, 'Frutos tropicales con hielo.Irresistible.', 8, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/jungle_q0hnjs.jpg', 'Frutal'),
-('Cerise', 'Cold Shades', 15, 'Cereza de la mas madura con hielo.', 9, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/cerise_vpbsp8.png', 'Frutal'),
-('Rasputin', 'Daruma E-liquid', 8, 'Fresa y frambuesa unidas al hielo artico!', 10, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427265/Productos/rasputin_ehzcgg.jpg', 'Frutal');
+INSERT INTO `liquidos` (`nombre`, `marca`, `precio`, `descripcion`, `id`, `imagenId`, `categoria`, `Cantidad`) VALUES
+('Don Juan Reserva', 'King\'s Crest', 22, 'Postre de galleta con chocolate y vainilla.No podrás resistir!\r\nLíquido ganador por dos veces consecutivas del premio en USA al mejor líquido de King Crest.', 1, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1666704030/Productos/don_juan_pjtms8.jpg', 'Postre', '100 ML'),
+('Baker\'s Daughter', 'Coil sill hand crafted', 18, 'Liquido a sabor Tarta de la abuela!!!', 2, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/bakersDaughter_tzalrr.png', 'Postre', '100 ML'),
+('Kastard', 'Tonino Vaper', 17, 'Crema pastelera con caramelo, vainilla y canela.', 3, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/kastard_rbuabs.jpg', 'Postre', '100 ML'),
+('Salted Caramel', 'Nitro\'s Col Brew', 19, 'Galletas cracker saladas con notas de helado de vainilla con una capa de caramelo fundido. Cremoso y dulce con un toque muy especial, descúbrelo.\r\n\r\n\r\n', 4, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427265/Productos/nitros_kzij3a.jpg', 'Postre', '100 ML'),
+('Jungle Wave', 'Malasia vaps', 13, 'Frutos tropicales con hielo.Irresistible.', 8, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/jungle_q0hnjs.jpg', 'Frutal', '100 ML'),
+('Cerise', 'Cold Shades', 15, 'Cereza de la mas madura con hielo.', 9, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427266/Productos/cerise_vpbsp8.png', 'Frutal', '100 ML'),
+('Rasputin', 'Daruma E-liquid', 8, 'Fresa y frambuesa unidas al hielo artico!', 10, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1647427265/Productos/rasputin_ehzcgg.jpg', 'Frutal', '50 ML'),
+('Vainara', 'Bombo', 17, 'Tiernos churros con azúcar de vainilla aderezados con un toque \r\nde canela. La devoción de los más golosos.', 221, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702587/Productos/vainara-50ml-bombo_zfs6bi.jpg', 'Postre', '50 ML'),
+('Trubio', 'Bombo', 17, 'Líquido con sabor a tabaco rubio con toques de nuez y barrica de Jerez, donde se aprecian claramente las distintas especias y el sabor a trufa.', 222, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702587/Productos/trubio-50ml-bombo_letoo7.jpg', 'Tabaquil', '50 ML'),
+('Purple on ice', 'Anarchist', 25, 'Anarchist Purple On Ice es un sabor verdaderamente jugoso y satisfactorio. Un equilibrio perfecto entre uvas maduras y chicle con un toque final helado..', 223, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702587/Productos/purple-on-ice-100ml-anarchist_aiyriz.jpg', 'Frutal', '100 ML'),
+('Bebeca', 'Atmos lab', 23, 'Este E-Líquido es uno de los sabores más conocidos de Atmos Lab. Sabor a tabaco suave con matices dulces y frutos secos.', 224, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/bebeca-50ml-atmos-lab_zqigg0.jpg', 'Tabaquil', '50 ML'),
+('Nutacco', 'Atmos lab', 23, 'Se trata de un sabor duradero, con deliciosas notas de frutos secos tostados. Es la combinación perfecta para cualquier bebida de alcohólica..', 225, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702587/Productos/nutacco-30ml-12mg-atmos-lab_hewtkg.png', 'Tabaquil', '50 ML'),
+('Custard cookies', 'Sadboy', 25, 'Sadboy E-Liquid Custard Cookie. Inhale la delicada galleta recién horneada y exhale el relleno de cremosos natillas.\r\n', 226, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/custard-cookie-100ml-sadboy_dsc6xb.jpg', 'Postre', '100 ML'),
+('Don cristo pistacho', 'Don cristo', 22, 'Un hermoso sabor clásico cubano que se ha beneficiado de 90 días de picado con una nota de pistacho que lo convierte en un líquido gourmet.\r\n', 227, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/don-cristo-pistachio-50ml-don-cristo_fpqoiz.jpg', 'Tabaquil', '50 ML'),
+('Bloody lime frooize', 'E-liquid France', 24, 'El líquido Bloody Lime de la gama Fruizee de Eliquid France te sorprenderá por su delicioso sabor a frutos rojos con un toque de lima verde, sobre un fondo de frescor.\r\n', 228, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/bloody-lime-50ml-fruizee_s0yw9f.jpg', 'Frutal', '50 ML'),
+('Blue vampire', 'Monster Club', 27, 'El líquido Blue Vampire de Monster Club te sorprenderá por su ireresistible fondo afrutado y fresco, ideal para vapear a diario.', 229, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/blue-vampire-450ml-monster-club_wtmqdb.jpg', 'Frutal', '450 ML'),
+('Hazelnut cream', 'Pachamama', 21, 'Pachamama Desserts Hazelnut Creme: e-líquido de 100ml al sabor de avellanas tostadas mezcladas a la perfección con una rica y dulce crema.', 230, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702586/Productos/hazelnut-creme-100ml-pachamama-desserts_qwxsv7.jpg', 'Postre', '100 ML'),
+('Subohm Apple Tobacco', 'Pachamama', 19, 'Todo el sabor de las manzanas variadas más dulces y tabaco. Perfecto para los que buscan un golpe de garganta suave ¡Un sabor increíble!', 231, 'https://res.cloudinary.com/dkfo76a1o/image/upload/v1668702587/Productos/apple-tobacoo-50ml-pachamama-subohm_pmpyud.jpg', 'Frutal', '50 ML');
 
 -- --------------------------------------------------------
 
@@ -209,7 +218,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `liquidos`
 --
 ALTER TABLE `liquidos`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=221;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=232;
 
 --
 -- AUTO_INCREMENT de la tabla `pedidos`
--- frontend/src/Components/Producto.js
+++ frontend/src/Components/Producto.js
@@ -1,30 +1,25 @@
 import React, { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { UserProvider, UserConsumer } from "../Context/CarritoContext";
 
 
-const Producto = ({id ,nombre, marca, descripcion, cantidad, precio}) => {
+const Producto = ({ id, nombre, marca, descripcion, cantidad, precio, imagenId }) => {
 
 	const carrito = useContext(UserConsumer);
     
     return(
-        <div className="card">
-                <div className="card-body text-muted">
-               	<h4 className="card-title">{nombre}</h4>
-                    <p>Precio: {precio} ‎€</p>
-                    <p>{descripcion}</p>
-                    <div className="row">
-                        <div className="col-sm-12">
-                            <div className="btn-group inline pull-left">
-                            	<Link to={"/producto/" + id} className="btn btn-danger btn-sm">Ver más</Link> 
-                            </div>
-                            <div className="btn-group inline pull-right">
-                            	<button className="btn btn-success btn-sm" onClick={() => carrito.addProduct({id ,nombre, marca, descripcion, cantidad, precio})}>Add <i className="fa fa-shopping-cart"></i></button>
-                            </div>
-                        </div>
-                    </div> 
-                </div>
-            </div>
+        <Card style={{ width: '17rem' }}>
+            <Card.Img variant="top" style={{ height: '275px' }} src={imagenId} />
+            <Card.Body>
+                <Card.Title>{nombre}</Card.Title>c
+                <Card.Subtitle className="mb-2 text-muted">Precio: {precio} ‎€</Card.Subtitle>
+                <Card.Text> {descripcion} </Card.Text>
+                <Button variant="danger" className="pull-left" href={"/producto/" + id}> Ver más </Button>
+                <Button variant="primary" className="pull-right" onClick={() => carrito.addProduct({ id, nombre, marca, descripcion, cantidad, precio })}><i className="fa fa-shopping-cart" /> Add </Button>
+            </Card.Body>
+        </Card>
         )
 };
 export default Producto;
